fdbd854daeccf2d0ed7a8b32a4473285a9f0f060
tsato1 <a8tb1117@gmail.com>
2022-09-18 16:14:52 +0800

    made repo private, upgraded all the dependencies in gradle files

--- app/src/main/java/com/tsato/mobile/jitpacktest/MainActivity.kt
+++ app/src/main/java/com/tsato/mobile/jitpacktest/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tsato.mobile.jitpacktest.ui.theme.JitpackTestTheme
+import com.tsato.mobile.mylibrary.MyObject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
+                    MyObject
                 }
             }
         }
